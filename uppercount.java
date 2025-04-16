fef96a659ab6c6f3423d50fa72e5356cc8bee640
gunal-n <gunalkavithanedunchezian@gmail.com>
2025-04-16 12:32:53 +0530

    Add uppercount.java

---
+++ uppercount.java
@@ -0,0 +1,12 @@
+class uppercount {
+    public static void main(String[] args){
+        StringBuffer sb = new StringBuffer("HEllo WE Are");
+        int count = 0;
+        for (int i = 0; i < sb.length(); i++){
+             if(Character.isUpperCase(sb.charAt(i))){
+                count++;
+             }
+        }
+        System.out.println(count);
+    }
+}
